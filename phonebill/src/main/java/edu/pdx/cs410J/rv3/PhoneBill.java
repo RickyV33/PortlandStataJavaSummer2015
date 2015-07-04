a32d4011491f98859f9424426ef391b6ce926da1
RickyV33 <3d.rickyV@gmail.com>
2015-07-03 18:34:02 -0700

    Create PhoneBill and PhoneCall classes

---
+++ phonebill/src/main/java/edu/pdx/cs410J/rv3/PhoneBill.java
@@ -0,0 +1,26 @@
+package edu.pdx.cs410J.rv3;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+import java.util.Collection;
+
+/**
+ * Created by Rickyy on 7/3/2015.
+ */
+public class PhoneBill extends AbstractPhoneBill{
+    @Override
+    public String getCustomer() {
+        return null;
+    }
+
+    @Override
+    public void addPhoneCall(AbstractPhoneCall call) {
+
+    }
+
+    @Override
+    public Collection getPhoneCalls() {
+        return null;
+    }
+}
---
+++ phonebill/src/main/java/edu/pdx/cs410J/rv3/PhoneCall.java
@@ -0,0 +1,28 @@
+package edu.pdx.cs410J.rv3;
+
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+/**
+ * Created by Rickyy on 7/3/2015.
+ */
+public class PhoneCall extends AbstractPhoneCall{
+    @Override
+    public String getCaller() {
+        return null;
+    }
+
+    @Override
+    public String getCallee() {
+        return null;
+    }
+
+    @Override
+    public String getStartTimeString() {
+        return null;
+    }
+
+    @Override
+    public String getEndTimeString() {
+        return null;
+    }
+}
