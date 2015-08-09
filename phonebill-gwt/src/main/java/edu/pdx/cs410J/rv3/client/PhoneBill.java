1a616569e9d020b04413cea1e9a2579b0b2afa2a
Ricky Valencia <3d.rickyV@gmail.com>
2015-08-08 18:17:00 -0700

    Add PhoneBill, PhoneCall, and PrettyPrinter to Project 5

--- phonebill-gwt/src/main/java/edu/pdx/cs410J/rv3/client/PhoneBill.java
+++ phonebill-gwt/src/main/java/edu/pdx/cs410J/rv3/client/PhoneBill.java
@@ -1,28 +1,95 @@
-package edu.pdx.cs410J.rv3.client;
+package edu.pdx.cs410J.rv3;
 
-import edu.pdx.cs410J.AbstractPhoneCall;
 import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
 
-import java.lang.Override;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
 
-public class PhoneBill extends AbstractPhoneBill
-{
-  private Collection<AbstractPhoneCall> calls = new ArrayList<AbstractPhoneCall>();
+/**
+ * The <code>PhoneBill</code> class extends the <code>AbstractPhoneBill</code> class which creates
+ * a bill consisting of a series of phone calls addressed to one person.
+ *
+ * @author Ricky Valencia
+ * @version 1.0
+ */
+public class PhoneBill extends AbstractPhoneBill {
+    /**
+     * Stores the customer's name that the phone calls belong to.
+     */
+    public String customer;
+    /**
+     * Stores the list of all the phone calls made by the customer.
+     */
+    public ArrayList<PhoneCall> phoneCalls;
 
-  @Override
-  public String getCustomer() {
-    return "CS410J";
-  }
+    /**
+     * Creates a new <code>PhoneBill</code> object using only the customers name.
+     *
+     * @param customer The name of the customer that the list of phone calls belongs to.
+     */
+    public PhoneBill(String customer) {
+        this.customer = customer;
+    }
 
-  @Override
-  public void addPhoneCall(AbstractPhoneCall call) {
-    this.calls.add(call);
-  }
+    /**
+     * Overrides <code>AbstractPhoneBill</code>'s abstract method <code>getCustomer</code> to return the customer's name.
+     *
+     * @return The customer's name
+     */
+    @Override
+    public String getCustomer() {
+        return customer;
+    }
 
-  @Override
-  public Collection getPhoneCalls() {
-    return this.calls;
-  }
+    /**
+     * Overrides <code>AbstractPhoneBill</code>'s abstract method <code>addPhoneCall</code> to add a phone call into
+     * the colletion of phone calls held by the customer.
+     *
+     * @param call A <code>PhoneCall </code> object to be stored into the list
+     */
+    @Override
+    public void addPhoneCall(AbstractPhoneCall call) {
+        if (phoneCalls == null) {
+            phoneCalls = new ArrayList<>();
+        }
+        phoneCalls.add((PhoneCall) call);
+    }
+
+    /**
+     * Overrides <code>AbstractPhoneBill</code>'s abstract method <code>getPhoneCalls</code> to return the sorted list of
+     * phone calls.
+     *
+     * @return The list of phone calls as a ArrayList.
+     */
+    @Override
+    public ArrayList<PhoneCall> getPhoneCalls() {
+        Collections.sort(phoneCalls);
+        return phoneCalls;
+    }
+
+    public ArrayList<PhoneCall> getPhoneCallsWithinRange(Date start, Date end) {
+        ArrayList<PhoneCall> rangeCalls = new ArrayList<>();
+
+        for (PhoneCall call : phoneCalls) {
+            Date callStart = call.getStartTime();
+            if (callStart.compareTo(start) >= 0 && callStart.compareTo(end) <= 0) {
+                rangeCalls.add(call);
+            }
+        }
+        return rangeCalls;
+    }
+
+    /**
+     * Returns <code>AbstractPhoneBill</code>'s method <code>toString</code> that prints a description of the customer's
+     * phone bill.
+     *
+     * @return The customer's name and the number of phone calls in his bill.
+     */
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
 }
--- phonebill-gwt/src/main/java/edu/pdx/cs410J/rv3/client/PhoneCall.java
+++ phonebill-gwt/src/main/java/edu/pdx/cs410J/rv3/client/PhoneCall.java
@@ -1,38 +1,145 @@
-package edu.pdx.cs410J.rv3.client;
+package edu.pdx.cs410J.rv3;
 
 import edu.pdx.cs410J.AbstractPhoneCall;
 
-import java.lang.Override;
+import java.text.DateFormat;
 import java.util.Date;
 
-public class PhoneCall extends AbstractPhoneCall
-{
+/**
+ * The <code>PhoneCall</code> class extends the <code>AbstractPhoneCall</code> class which
+ * creates a phone call with the caller's phone number, callee's phone number, start time of the call, and end time of
+ * the call. It also implements the <code>Comparable</code> interface so we it can be used to compare two different phone
+ * calls for sorting.
+ *
+ * @author Ricky Valencia
+ * @version 1.0
+ */
+public class PhoneCall extends AbstractPhoneCall implements Comparable<PhoneCall> {
+    /**
+     * Stores the caller's phone number in the format xxx-xxx-xxxx.
+     */
+    private String caller;
+    /**
+     * Stores the callee's phone number in the format xxx-xxx-xxxx.
+     */
+    private String callee;
+    /**
+     * Stores the start date and time of the phone call in the format MM/dd/yyyy hh:mm (am/pm).
+     */
+    private Date startTime;
+    /**
+     * Stores the end date and time of the phone call in the format MM/dd/yyyy hh:mm (am/pm).
+     */
+    private Date endTime;
 
-  @Override
-  public String getCaller() {
-    return "123-345-6789";
-  }
 
-  @Override
-  public Date getStartTime() {
-    return new Date();
-  }
+    /**
+     * Creates a new <code>PhoneCall</code> object using a caller, callee, start time, and end time as the initial times.
+     *
+     * @param caller    The phone number of the person who made the phone call.
+     * @param callee    The phone number of the person who received the phone call
+     * @param startTime The start date and time of the phone call.
+     * @param endTime   The end date and time of the phone call.
+     */
+    public PhoneCall(String caller, String callee, Date startTime, Date endTime) {
+        this.caller = caller;
+        this.callee = callee;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
-  public String getStartTimeString() {
-    return "START " + getStartTime();
-  }
+    /**
+     * Overrides <code>AbstractPhoneCall</code>'s abstract method <code>getCaller</code> to get the caller's phone number.
+     *
+     * @return caller The phone number of the caller as a string from the object.
+     */
+    @Override
+    public String getCaller() {
+        return caller;
+    }
 
-  @Override
-  public String getCallee() {
-    return "345-677-2341";
-  }
+    /**
+     * Overrides <code>AbstractPhoneCall</code>'s abstract method <code>getCallee</code> to get the callee's phone number.
+     *
+     * @return callee The phone number of the callee as a string from the object.
+     */
+    @Override
+    public String getCallee() {
+        return callee;
+    }
 
-  public Date getEndTime() {
-    return new Date();
-  }
+    /**
+     * Overrides <code>AbstractPhoneCall</code>'s abstract method <code>getStartTimeString</code> to get the start date
+     * and the time from the object.
+     *
+     * @return The phone number of the callee as a string.
+     */
+    @Override
+    public String getStartTimeString() {
+        return DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(startTime);
+    }
 
-  public String getEndTimeString() {
-    return "END " + getEndTime();
-  }
+    /**
+     * Overrides <code>AbstractPhoneCall</code>'s abstract method <code>getEndTimeString</code> to get the end date
+     * and the time from the object.
+     *
+     * @return The phone number of the callee as a string.
+     */
+    @Override
+    public String getEndTimeString() {
+        return DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(endTime);
+    }
 
+    /**
+     * Overrides <code>AbstractPhoneCall</code>'s abstract method <code>getEndTime</code> to get the end date and the
+     * time as a <code>Date</code> object.
+     *
+     * @return The date object that holds the end date and time of the phone call.
+     */
+    @Override
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    /**
+     * Overrides <code>AbstractPhoneCall</code>'s abstract method <code>getStartTime</code> to get the start date and the
+     * time as a <code>Date</code> object.
+     *
+     * @return The date object that holds the start date and time of the phone call.
+     */
+    @Override
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    /**
+     * Returns <code>AbstractPhoneCall</code>'s method <code>toString</code> that prints a description of the phone call
+     * that took place between two phone numbers with dates and times.
+     *
+     * @return Returns a small description of the phone call.
+     */
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    /**
+     * Overrides <code>Comparable</code>'s method to compare two seperate phone calls with each other. They are being compared
+     * based on their start time, but if they share the same start time, then they are sorted by their phone call.
+     *
+     * @param call The next <code>PhoneCall</code> object that is being compared to the current object.
+     * @return Returns -1 if <code>this</code> comes before <code>call</code>, 1 if it comes after, or 0 if they are equal.
+     */
+    @Override
+    public int compareTo(PhoneCall call) {
+        Date startTime = call.getStartTime();
+        String caller = call.getCaller();
+
+        //Compares the begin time
+        if (this.startTime.equals(startTime)) {
+            return (this.caller.compareTo(caller));
+        }
+
+        return this.startTime.compareTo(startTime);
+    }
 }
